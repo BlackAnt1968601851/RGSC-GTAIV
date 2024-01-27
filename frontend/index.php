ba7f5f1894bf93a0857f83ccc897e07a892b5a87
BlackAnt1968601851 <117094394+BlackAnt1968601851@users.noreply.github.com>
2024-01-27 03:18:15 -0500

    Updated the code even more.
    added the ability to use any account.
    replaced the email with a actual username for logging in.

--- frontend/index.php
+++ frontend/index.php
@@ -1,3 +1,14 @@
 <?php
-	header("location:./ingame/start")
+	print('
+<html>
+<head>
+<title>Welcome to Social Club</title>
+</head>
+<body>
+<h1> Welcome to Social Club Replacement Server.</h1>
+</br>
+This is a test server. please do not visit this site.
+</body>
+</html>
+')
 ?>
